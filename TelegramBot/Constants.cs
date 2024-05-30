740db6903d749178ee677cae6c2a4d52773b21b5
Андрій <repinandrey98335@gmail.com>
2024-05-30 04:14:29 +0300

    Constants

---
+++ TelegramBot/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace TelegramBot
+{
+    internal class Constants
+    {
+        public static readonly string Token = "6657591558:AAEbn5jAjE_mt6nrgFXChx81tI21SlthBBc";
+        public static readonly string BaseURL = "https://localhost:7289";
+    }
+}
